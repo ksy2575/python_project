bccd11c542660df94ba6fed2c7371fa7a3c87d21
DngDae <dngdea@gmail.com>
2021-05-22 17:22:11 +0900

    21.05.22 zero commit

--- LostArk_P&L_Calculator/main.py
+++ LostArk_P&L_Calculator/main.py
@@ -5,10 +5,15 @@ chromedriver = '.\chromedriver\chromedriver.exe'
 # selenum의 webdriver에 앞서 설치한 chromedirver를 연동한다.
 driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
-driver.get('https://auto.naver.com/bike/mainList.nhn')
+driver.get('https://lostark.game.onstove.com/Market')
+def calculation():
 
+driver.find_element_by_xpath('/html/body/div[2]/div/main/div/div[2]/div[2]/form/fieldset/div/div[1]/input').send_keys('asdfasdf')
+results = driver.find_elements_
 print("+" * 100)
 print(driver.title)   # 크롤링한 페이지의 title 정보
 print(driver.current_url)  # 현재 크롤링된 페이지의 url
 print("바이크 브랜드 크롤링")
 print("-" * 100)
+
+<em data-grade="0">1</em>
